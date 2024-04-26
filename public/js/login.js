cb14e440029aa9386d3cbde171b4e0b012bbc7a1
Amlend <amlendsingh@gmail.com>
2024-04-26 23:56:39 +0530

    added-login-signup-api

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault(); // Prevent default form submission
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await axios.post("/login/validiation", {
+      email,
+      password,
+    });
+
+    if (response.data.success) {
+      localStorage.setItem("token", response.data.token);
+      window.location.href = "";
+    } else {
+      alert("Invalid login credentials.");
+    }
+  } catch (error) {
+    console.error("Login error:", error);
+    alert("An error occurred during login. Please try again.");
+  }
+});
